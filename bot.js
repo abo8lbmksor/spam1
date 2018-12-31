45397e9127db6652e1f095e898814ed730e311f3
abo8lbmksor <40383533+abo8lbmksor@users.noreply.github.com>
2018-12-31 19:12:36 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,32 +1,34 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '.'
+var prefix = "1";
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
-  console.log('')
-  console.log('')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log(`[Start] ${new Date()}`);
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log(`channels! [ " ${client.channels.size} " ]`);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════]╗')
-  console.log(' Bot Is Online')
-  console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
+  console.log(`Logged in as ${client.user.tag}!`);  
 });
 
-client.login(process.env.BOT_TOKEN);
+client.on('ready', async() => {
+var server = "500316886265888790"; // ايدي السررفر
+var channel = "500318605444120622";//ايدي الروم
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('**éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,éMyyy ,**')
+    },305);
+})
 
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command == "say") {
+if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('?|**\`ADMINISTRATOR\`ليس لديك صلاحيات`**');
+   message.channel.sendMessage(args.join("  "))
+   message.delete()
+  }
+ });
+
+ 
+client.login(process.env.BOT_TOKEN);
